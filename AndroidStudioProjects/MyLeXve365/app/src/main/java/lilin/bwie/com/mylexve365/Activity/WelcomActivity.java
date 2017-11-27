f51196239ac422cda5bcc44b3891a6a4c652299e
unknown <lilin0719@163.com>
2017-11-27 21:27:22 +0800

    MyCodeDemo commit

--- AndroidStudioProjects/MyLeXve365/app/src/main/java/lilin/bwie/com/mylexve365/Activity/WelcomActivity.java
+++ AndroidStudioProjects/MyLeXve365/app/src/main/java/lilin/bwie/com/mylexve365/Activity/WelcomActivity.java
@@ -1,13 +1,17 @@
-package lilin.bwie.com.mylexve365;
+package lilin.bwie.com.mylexve365.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import lilin.bwie.com.mylexve365.R;
+
+public class WelcomActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
